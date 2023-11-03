5d85f6aaa32bb3ece9f414f55c365af31c775a7e
manish-bhukar <manish20223142@gmail.com>
2023-11-03 15:02:55 +0530

    Login page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import './App.css';
-import ProductList from './features/product-list/productList';
+
+
 import Home from './Pages/Home';
+import LoginPage from './Pages/LoginPage';
+import SignupPage from './Pages/SignupPage';
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Home></Home>
+    ),
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+])
 function App() {
   return (
     <div className="App">
-      <Home />
+       <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,10 @@
+import Login from "../features/Auth/Login";
+
+export default function LoginPage(){
+    return (
+        <div>
+<Login></Login>
+            
+        </div>
+    )
+}
