437e8f5ce0c45911d5e92686cd930a65ede65a83
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-11-03 11:34:17 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ProductList from './features/product-list/productList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
