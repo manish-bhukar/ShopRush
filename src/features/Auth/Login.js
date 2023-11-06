1d46d93ed097797d99d0145476e04c018f3cbc95
manish-bhukar <manish20223142@gmail.com>
2023-11-06 11:06:50 +0530

    Signup and login backend

--- src/features/Auth/Login.js
+++ src/features/Auth/Login.js
@@ -1,7 +1,21 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
+import { checkUserAsync, selectError, selectloggedInUser } from "./authSlice"
+import { useForm } from "react-hook-form"
+import { useDispatch, useSelector } from "react-redux"
+
 export default function Login(){
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const error=useSelector(selectError)
+  const user=useSelector(selectloggedInUser);
+  const dispatch=useDispatch();
     return (
         <div>
+          {user &&<Navigate to='/' replace={true}></Navigate>}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -15,23 +29,26 @@ export default function Login(){
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form  noValidate onSubmit={handleSubmit((data)=>{
+         dispatch(checkUserAsync({email:data.email,password:data.password}));
+          })} className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
               <div className="mt-2">
-                <input
+              <input
                   id="email"
-                  name="email"
+                  {...register("email",{required:"email is required",pattern:{value:/\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/gi,message:'email not valid'}})}
                   type="email"
-                  autoComplete="email"
-                  required
+                  
+                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-              </div>
+              
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
             </div>
-
+            </div>
             <div>
               <div className="flex items-center justify-between">
                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
@@ -44,15 +61,18 @@ export default function Login(){
                 </div>
               </div>
               <div className="mt-2">
-                <input
+              <input
                   id="password"
-                  name="password"
+                  {...register("password",{required:"password is required"})}
                   type="password"
-                  autoComplete="current-password"
-                  required
+                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+              {errors.password &&  <p className="text-red-500">{errors.password.message}</p>}
+            
               </div>
+              {error &&  <p className="text-red-500">{error.message}</p>}
+            
             </div>
 
             <div>
---
+++ src/features/Auth/Protected.js
@@ -0,0 +1,10 @@
+import { UseSelector, useSelector } from "react-redux";
+import { selectloggedInUser } from "./authSlice";
+import { Navigate } from "react-router-dom";
+export default function Protected({children}){
+    const user=useSelector(selectloggedInUser)
+    if(!user){
+        return <Navigate to='/login' replace={true}></Navigate>
+    }
+    return children;
+}
