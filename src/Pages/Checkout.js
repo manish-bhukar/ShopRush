4e4ec88abe6b9fa7363ba16dae4c529e46136330
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2023-11-06 16:43:48 +0530

    fix checkout page

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -412,36 +412,6 @@ function Checkout() {
             </div>
 
             <div className="border-t border-gray-200 px-4 py-6 sm:px-6 bg-white">
-              <div className="flex justify-between text-base font-medium text-gray-900">
-                <p>Subtotal</p>
-                <p>$262.00</p>
-              </div>
-              <p className="mt-0.5 text-sm text-gray-500">
-                Shipping and taxes calculated at checkout.
-              </p>
-              <div className="mt-6">
-                <Link
-                  to="/pay"
-                  className="flex items-center justify-center rounded-md border border-transparent bg-blue-800 py-3 text-white"
-                >
-                  Pay Now
-                </Link>
-              </div>
-              <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                <p>
-                  or
-                  <Link to="/">
-                    <button
-                      type="button"
-                      className="font-medium text-indigo-600 hover:text-indigo-500"
-                      onClick={() => setOpen(false)}
-                    >
-                      Continue Shopping
-                      <span aria-hidden="true"> &rarr;</span>
-                    </button>
-                  </Link>
-                </p>
-              </div>
             </div>
           </div>
         </div>
