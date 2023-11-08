0a687f2e032ccabd6ea8ec401c81f4e7199c6fab
manish-bhukar <manish20223142@gmail.com>
2023-11-08 13:46:38 +0530

    order success

--- src/features/Auth/Signup.js
+++ src/features/Auth/Signup.js
@@ -15,7 +15,7 @@ export default function Signup(){
   const user=useSelector(selectloggedInUser)
     return(
         <div>
-         {user &&<Navigate to='/login' replace={true}></Navigate>}
+         {user &&<Navigate to='/' replace={true}></Navigate>}
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -96,7 +96,9 @@ export default function Signup(){
                   {...register("confirmPassword",{required:" confirm password is required",
                   validate: (value, formValues) => value === formValues.password||'password not matching'
                 })}
-                  type="confirmPassword"
+                // type="confirmPassword"
+                  type="password"
+
                  
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -105,12 +107,14 @@ export default function Signup(){
             </div>
 
             <div>
+              <Link to="/login">
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign up
               </button>
+              </Link>
             </div>
           </form>
 
