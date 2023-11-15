d92701cf611ad2ff750ff5b663a14e995039fcb9
Mangal Gupta <127006680+mangalgithub@users.noreply.github.com>
2023-11-15 16:46:16 +0530

    Delete src directory

--- src/Pages/CartPage.js
+++
@@ -1,8 +0,0 @@
-import Cart from "../features/cart/cart"
-export default function CartPage(){
-    return (
-<div>
-    <Cart></Cart>
-</div>
-    )
-}
--- src/Pages/ForgotPassword.js
+++
@@ -1,8 +0,0 @@
-import ForgotPassword from "../features/Auth/ForgotPassword";
-export default function forgot() {
-  return (
-    <div>
-      <ForgotPassword></ForgotPassword>
-    </div>
-  );
-}
